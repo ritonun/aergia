977dfee8d1caafa07d707d39997af86ecfd9113b
Ritonun <jeremy.moreau2001@gmail.com>
2023-02-25 20:19:13 +0100

    add new test

--- tests/test_saveload.py
+++ tests/test_saveload.py
@@ -37,3 +37,14 @@ class TestSaveLoadSystem:
         s1 = s.get_path("save1")
         result = "temp/save1.json"
         assert s1 == result
+    
+    def test_save_and_load(self):
+        create_temp_folder()
+        file_dict = {"save1": "save1.json", "save2": "save2.json"}
+        s = SaveLoadSystem("temp/", file_dict)
+        data = {"var": 1.0}
+        s.save_data("save1", data)
+        loaded_data = s.load_file("save1")
+        delete_temp_folder()
+        assert loaded_data["var"] == 1.0        
+
