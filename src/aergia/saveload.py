738daea74df47e9f0dd3951a4b2a4eb551c68f49
Ritonun <jeremy.moreau2001@gmail.com>
2023-03-05 19:37:50 +0100

    add mkdocs autodocs

--- src/aergia/saveload.py
+++ src/aergia/saveload.py
@@ -2,6 +2,13 @@ import json
 
 
 class SaveLoadSystem:
+    """System to handle Save and Loading of JSON File
+
+    Attributes:
+        files (dict): contain all file path
+        save_folder (str): path to folder containing all the files
+    """
+
     def __init__(self, save_folder, files_dict):
         self.save_folder = save_folder
         self.files = files_dict
@@ -20,12 +27,26 @@ class SaveLoadSystem:
                 f.close()
 
     def load_file(self, file_key):
+        """Load a .json file into a dict
+
+        Args:
+            file_key (str): file name in the self.files dict
+
+        Returns:
+            dict: Return the data contains in the .json file
+        """
         data = None
         with open(self.get_path(file_key), "r") as f:
             data = json.load(f)
         return data
 
     def save_data(self, file_key, data: dict):
+        """Save a dict into a .json file
+
+        Args:
+            file_key (str): file name in the self.files dict
+            data (dict): Data to save into the file
+        """
         with open(self.get_path(file_key), "w") as f:
             json.dump(data, f, indent=2)
 
